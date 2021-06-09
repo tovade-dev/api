7eeccfef4c2788959a4630369f7700cfb4b96e05
tovade <66245404+tovade@users.noreply.github.com>
2021-06-09 16:53:21 +0200

    :tada: more info endpoints

--- src/routes/info.js
+++ src/routes/info.js
@@ -9,6 +9,26 @@ route.get("/covid", async (req, res) => {
 
   return res.json(rese);
 });
+const { fetchPkg, fetchSub } = require("../utils/functions");
+route.get("/npm", async (req, res) => {
+  if (!req.query.package)
+    return res.json({
+      error: true,
+      message: "No npm package was provided in the query",
+    });
+  const data = await fetchPkg(req.query.package);
+  return res.json(data);
+});
+route.get("/reddit", async (req, res) => {
+  if (!req.query.sub)
+    return res.json({
+      error: true,
+      message:
+        "Missing subreddit! example: https://api.tovade.xyz/v1/info/reddit?sub=discord ",
+    });
+  const data = await fetchSub(req.query.sub);
+  return res.json(data);
+});
 
 module.exports = {
   endpoint: "/info",
---
+++ src/utils/functions.js
@@ -0,0 +1,56 @@
+const fetch = require("node-fetch");
+const moment = require("moment");
+async function fetchPkg(pkg) {
+  const res = await fetch(
+    `https://registry.npmjs.com/${encodeURIComponent(pkg)}`
+  ).catch();
+  if (res.status === 404)
+    return { error: true, message: "Package does not exist." };
+  const body = await res.json();
+  return {
+    description: body.description || "No description.",
+    version: body["dist-tags"].latest,
+    license: body.license || "None",
+    author: body.author ? body.author.name : "Unknown User.",
+    createdAt: moment.utc(body.time.created).format("YYYY/MM/DD hh:mm:ss"),
+    last_modify: body.time.modified
+      ? moment.utc(body.time.modified).format("YYYY/MM/DD hh:mm:ss")
+      : "has never been modified",
+    repository: body.repository
+      ? `[View Here](${body.repository.url.split("+")[1]})`
+      : "None",
+    maintainers: body.maintainers.map((user) => user.name).join(", "),
+  };
+}
+async function fetchSub(sub) {
+  const dataRes = await fetch(
+    `https://reddit.com/r/${sub}/new.json?limit=1`
+  ).then((rese) => rese.json());
+  if (dataRes.statusCode === 404) return "Invalid subreddit?";
+  const post = dataRes.data.children[0];
+  const p = post.data;
+  return {
+    title: p.title || "",
+    url: p.url || "",
+    thumbnail: p.thumbnail,
+    published: moment.utc(p.created_utc).format("YYYY/MM/DD hh:mm:ss"),
+    permalink: `https://reddit.com${p.permalink}`,
+    author: {
+      name: p.author || "Deleted",
+    },
+    sub: {
+      name: p.subreddit || sub,
+    },
+    extra: {
+      score: p.score,
+    },
+    votes: {
+      upvotes: p.ups,
+      downvotes: p.downs,
+    },
+  };
+}
+module.exports = {
+  fetchPkg,
+  fetchSub,
+};
