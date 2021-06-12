5a2c442020d8443c34467a76afa7b122dc0691ff
tovade <66245404+tovade@users.noreply.github.com>
2021-06-12 11:01:30 +0200

    :tada: fix bugs

---
+++ src/bot/commands/info.js
@@ -0,0 +1,38 @@
+const fetch = require("node-fetch");
+module.exports = {
+  name: "info",
+  description: "Info",
+  async execute(client, message, args) {
+    const res = await fetch(
+      "https://api.tovade.xyz/v1/admin/user/info?userID=" + message.author.id,
+      {
+        method: "GET",
+        headers: {
+          auth: client.config.adminkey,
+        },
+      }
+    ).then((r) => r.json());
+    if (!res) return message.channel.send(`You are not registered.`);
+    if (res.error) return message.channel.send(res.message);
+
+    const toSend = {
+      content: `Your stats`,
+      embed: {
+        fields: [],
+        color: 0xffa500,
+      },
+    };
+
+    toSend.embed.fields.push({
+      name: "Request",
+      value: `Total: ${res.stats.total}`,
+      inline: true,
+    });
+    toSend.embed.fields.push({
+      name: "Rate-limit",
+      value: `Max: ${res.ratelimit.max}\nUsed: ${res.ratelimit.used}`,
+      inline: true,
+    });
+    message.channel.send(toSend);
+  },
+};
