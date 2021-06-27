e24b0d028ec2dc5de968439d879b89a6adc1865f
tovade <gamingethan650@gmail.com>
2021-06-27 10:55:54 +0200

    :hammer: chore: update example config

--- config.example.js
+++ config.example.js
@@ -1,4 +1,10 @@
 module.exports = {
   port: 3000, //the port for the api to run on.
   servers: ["http://localhost:3000"], //the servers for the docs
+  genius: "", //genius api key
+  discord: "https://discord.gg/u8NgtppkRp", //your discord url
+  mongo: "", // Your mongoDB url
+  discord_token: "", //Your discord token
+  adminkey: "admin", //api admin key
+  admins: ["556218750337286145", "853232801398128640"], //admin user id's
 };
